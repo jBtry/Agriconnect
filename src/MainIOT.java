b8406f25416fdb04f5b02483dd07393b21687d79
jBtry <119407693+jBtry@users.noreply.github.com>
2024-02-14 17:32:01 +0100

    Fin conception

--- src/MainIOT.java
+++ src/MainIOT.java
@@ -14,9 +14,9 @@ public class MainIOT {
 
     /**
      * Crée 5 capteurs, affiche leurs informations
-     * Récupère le gestionnaire (objet) distant et l'affecte aux 5 cpateurs
+     * Récupère le gestionnaire (objet) distant et l'affecte aux 5 capteurs
      * Lance un relevé toutes les 5 secondes sur les 5 capteurs
-     * Au bout de 10 relevé on arrête.
+     * Au bout de 10 relevés, on arrête.
      * On vide la table contenant la liste des capteurs sur le serveur
      * Le programme s'arrête
      * @param args non utilisé
