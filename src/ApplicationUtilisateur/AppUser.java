e58b327a38d58fb6859e4d42f19d4b86e7c6ae1e
jBtry <119407693+jBtry@users.noreply.github.com>
2024-03-03 19:02:45 +0100

    Conception ajustement

--- src/ApplicationUtilisateur/AppUser.java
+++ src/ApplicationUtilisateur/AppUser.java
@@ -2,5 +2,69 @@ package ApplicationUtilisateur;
 
 public abstract class AppUser {
 
-    /**  */
+    /**
+     * Permet à l'utilisateur d'ajouter/enregistrer un capteur
+     * dans la base de donnée du Gestionnaire
+     */
+    public void ajouterCapteur(String idCapteur) {
+
+    }
+
+    /**
+     * Permet de commencer l'enregistrement de relevé
+     * @param id
+     */
+    public void demarrerCapteur(String id) {
+
+    }
+
+    /**
+     * Permet d'arrêter l'enregistrement de relevé
+     * @param id
+     */
+    public void stopperCapteur(String id) {
+
+    }
+
+    /**
+     *
+     * @param idCapteur
+     */
+    public void retirerCapteur(String idCapteur) {
+
+    }
+
+    /**
+     *
+     */
+    public void listeCapteurs() {
+
+    }
+
+    /**
+     *
+     * @param idCapteur
+     */
+    public void obtenirDernierReleve(String idCapteur) {
+
+    }
+
+    /**
+     *
+     * @param idCapteur
+     * @param duree
+     */
+    public void statsCapteurs(String idCapteur, int duree) {
+
+    }
+
+    /**
+     *
+     * @param intervalle
+     * @param idCapteur
+     */
+    public void modifierIntervalle(int intervalle, String idCapteur) {
+
+    }
+
 }
