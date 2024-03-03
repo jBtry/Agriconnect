fa37049c1b195f5784be78321fac4f3f424e131f
jBtry <119407693+jBtry@users.noreply.github.com>
2024-03-03 17:42:26 +0100

    Code MainAppUser 70%

--- src/ApplicationUtilisateur/Textes.java
+++ src/ApplicationUtilisateur/Textes.java
@@ -7,8 +7,25 @@ package ApplicationUtilisateur;
 public abstract class Textes {
 
     /** Message d'accueil */
-    protected static final String ACCUEIL = "Bienvenue  sur l'application Agriconnect ! ";
+    public static final String ACCUEIL = "***************************************************\n" +
+                                         "*            Bienvenue sur Agriconnect !          *\n" +
+                                         "***************************************************";
 
     /** Menu des fonctionnalités */
-    protected static final String ACCUEIL = "Bienvenue  sur l'application Agriconnect ! ";
+    public static final String FCT = "1 - Ajouter un capteur\n" +
+            "2 - Démarrer un capteur\n" +
+            "3 - Stopper un capteur\n" +
+            "4 - Retirer un capteur\n" +
+            "5 - Lister les capteurs\n" +
+            "6 - Voir le dernier relevé d'un capteur\n" +
+            "7 - Obtenir des statistiques sur les relevés (moyenne et tendances)\n" +
+            "8 - Modifier l'intervalle de mesure pour un ou tous les capteurs\n" +
+            "9 - Quitter Agriconnect";
+
+    /** Demande de faire un choix à l'utilisateur */
+    public static final String CHOIX = "Que souhaitez-vous faire ? ";
+
+    /** Message d'erreur lors de la saisi utilisateur */
+    public static final String ERR_SAISI= "Vous devez saisir un chiffre compris entre 1 et 9 ";
+
 }
--- src/MainAppUser.java
+++ src/MainAppUser.java
@@ -1,3 +1,7 @@
+import ApplicationUtilisateur.Textes;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class MainAppUser {
     /**
@@ -13,23 +17,50 @@ public class MainAppUser {
      * - Modifier l'intervalle de mesure pour un ou bien tous les capteurs
      * @param args non utilisés
      */
+
+    /* Pour la saisie utilisateur */
+    private static final Scanner clavier = new Scanner(System.in);
     public static void main(String args[]) {
-
+        int choix;
+        MainAppUser.afficher(Textes.ACCUEIL);
+        while(true) {
+            MainAppUser.afficher(Textes.FCT);
+            choix = MainAppUser.demanderChoix();
+            switch (choix) {
+                case 1 -> ;
+                case 2 -> ;
+                case 3 -> ;
+                case 4 -> ;
+                case 5 -> ;
+                case 6 -> ;
+                case 7 -> ;
+                case 8 -> ;
+                case 9 ->  ;
+                default -> MainAppUser.afficher(Textes.ERR_SAISI);
+            }
+        }
     }
-
-
-
 
 
     /* Demande un choix à l'utilisateur */
     private static int demanderChoix() {
+        MainAppUser.afficher(Textes.CHOIX);
+        int choix = 0;
+        do {
+            try {
+                choix = clavier.nextInt();
+            } catch (InputMismatchException err) {
+                MainAppUser.afficher(Textes.ERR_SAISI);
+                clavier.next(); // Important pour consommer l'entrée non valide et éviter une boucle infinie
+            }
+        } while(choix == 0);
 
-        return 0;
+        return choix;
     }
 
-
     /* Affiche les menus de l'application */
-    private static void afficher(String ) {
+    private static void afficher(String texte) {
+        System.out.println(texte+"\n");
 
     }
 }
